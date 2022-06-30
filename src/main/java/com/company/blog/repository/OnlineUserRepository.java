513dcc54d97a2f3bb8f35f606e9a93e52bd3e012
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-06-30 17:13:46 +0530

    Find users by country

--- src/main/java/com/company/blog/repository/OnlineUserRepository.java
+++ src/main/java/com/company/blog/repository/OnlineUserRepository.java
@@ -2,8 +2,12 @@ package com.company.blog.repository;
 
 import com.company.blog.entity.OnlineUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface OnlineUserRepository extends JpaRepository<OnlineUser, Long> {
+    List<OnlineUser> findByLocationCountry(String country);
 }
