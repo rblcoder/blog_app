e0d68f8050a6e5fb4b809a87cc523cb7bd155cb0
rblcoder <35212670+rblcoder@users.noreply.github.com>
2022-06-30 12:26:20 +0530

    specify @Param in native query in PostRepository

--- src/main/java/com/company/blog/repository/PostRepository.java
+++ src/main/java/com/company/blog/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.company.blog.entity.OnlineUser;
 import com.company.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -13,5 +14,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query(value = "select p.* from post p inner join online_user u on p.author_id = u.id and u.name=:name",
             nativeQuery = true)
-    List<Post> findAllPostsByOnlineUserName(String name);
+    List<Post> findAllPostsByOnlineUserName(@Param("name") String name);
 }
